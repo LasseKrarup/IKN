ebf4745a523626819b58a3fe58dfc9b9b8205e0d
Lasse Krarup <au579525@uni.au.dk>
2019-03-05 16:23:00 +0100

    Fixed issue which came from incorrect truncation of received string

--- file_client.cs
+++ file_client.cs
@@ -2,53 +2,89 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.IO;
 
 
 namespace TcpFileClient{
-    public class MyTcpClient{
+    public class MyTcpClient
+    {
         private static int port = 9000;
         private static TcpClient client = null;
         private static byte[] outputBuffer = new byte[1024];
         private static string outputString = null;
         private static byte[] inputBuffer = new byte[4096];
-        private static string responseData = null;
+        private static string receivedString = null;
  
-        public static void Main(string[] args) {
+        public static void Main(string[] args) 
+        {
             string serverIP = args[0];
             string filepath = args[1];
 
-            Console.WriteLine("Attempting to get file '{0}' from IP {1}", filepath, serverIP);
-            Connect(serverIP, filepath);
+            Console.WriteLine("Attempting to connect to server at IP {0}", serverIP);
+            Connect(serverIP);
+
+            Console.WriteLine("Requesting file {0} from server", filepath);
+            GetFile(filepath);
         }
 
-        static void Connect(string server, string message) 
+        private static TcpClient Connect(string server) 
+        {
+            try
             {
-                try
-                {
-                    client = new TcpClient(server, port);   
-                }
-                catch (System.Exception e)
-                {
-                    Console.WriteLine("Failed to connect: {0}", e.ToString());
-                }
-                
-                // Encode string to bytes
-                outputBuffer = System.Text.Encoding.ASCII.GetBytes(message);    
-
-                // Set up the stream (to read and write from)
-                NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer. 
-                stream.Write(outputBuffer, 0, outputBuffer.Length);
-
-                Console.WriteLine("Requested file.");         
-
-                // Read the first batch of the TcpServer response bytes.
-                
-                stream.Read(inputBuffer, 0, inputBuffer.Length);
-                
-                responseData = System.Text.Encoding.ASCII.GetString(inputBuffer);
-                Console.WriteLine("Received: {0}", responseData);
+                client = new TcpClient(server, port);   
             }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("Failed to connect: {0}", e.ToString());
+            }
+
+            return client;
+        }
+
+        private static void GetFile(string path)
+        {
+            // Encode string to bytes
+            outputBuffer = System.Text.Encoding.ASCII.GetBytes(path);    
+
+            // Set up the stream (to read and write from)
+            NetworkStream stream = client.GetStream();
+
+            // Send the request to the connected TcpServer. 
+            stream.Write(outputBuffer, 0, outputBuffer.Length);
+
+            Console.WriteLine("Requested file.");         
+        
+            // Read filesize from stream
+            int bytesRead = stream.Read(inputBuffer, 0, inputBuffer.Length);
+
+            // To string
+            receivedString = System.Text.Encoding.ASCII.GetString(inputBuffer,0,bytesRead);
+            Console.WriteLine("Received: {0}", receivedString);
+
+            if (receivedString == "0") { //Means that file doesn't exist or is empty
+                Console.WriteLine("File requested doesn't exist or is empty");
+            } else {
+                Console.WriteLine("File {0} exists and is {1} bytes large",path,receivedString);
+
+                outputBuffer = Encoding.ASCII.GetBytes("1");
+                stream.Write(outputBuffer,0,outputBuffer.Length); //ack
+                
+                // Create new byte array that is the size of the incoming file
+                byte[] truncatedBuffer = new byte[Convert.ToInt32(receivedString)];
+
+                bytesRead = stream.Read(truncatedBuffer,0,truncatedBuffer.Length);
+                //receivedString = Encoding.ASCII.GetString(inputBuffer,0,bytesRead);
+
+                File.WriteAllBytes("received_"+path,truncatedBuffer);
+                //Console.WriteLine(receivedString);
+                Console.WriteLine("Wrote to file");
+            }
+        }
+
+        private static Boolean SendAck()
+        {
+            outputBuffer = Encoding.ASCII.GetBytes("1");
+            stream.Write(outputBuffer,0,outputBuffer.Length); //ack
+        }
     }
 }
--- file_server.cs
+++ file_server.cs
@@ -10,9 +10,11 @@ namespace TcpFileServer{
     public class MyTcpServer{
         private static int port = 9000;
         private static TcpListener server = null;
-        private static byte[] recBuffer = new byte[1024];
+        private static byte[] recBuffer = new byte[1000];
         private static string receivedString = null;
         private static byte[] outputFile = new byte[4096];
+        private static byte[] outBuf = new byte[1000];
+        private static string outMsg = null;
  
         public static void Main(string[] args) {
             SetupServer();
@@ -48,28 +50,35 @@ namespace TcpFileServer{
             TcpClient clientHandler = server.AcceptTcpClient(); //returns the connected TCP client object
 
             NetworkStream dataStream = clientHandler.GetStream();
-            dataStream.Read(recBuffer, 0, 1000); //buffer, place to start in buffer, number of bytes read
-            receivedString = Encoding.ASCII.GetString(recBuffer);
-            Console.WriteLine(receivedString);
+            int bytesRead = dataStream.Read(recBuffer, 0, 1000); //buffer, place to start in buffer, max number of bytes to read
+            receivedString = Encoding.ASCII.GetString(recBuffer, 0, bytesRead);
 
-            Console.WriteLine(receivedString);
-            if(receivedString == "test.txt") {
-                Console.WriteLine("Yes");
-            } else {
-                Console.WriteLine("No");
-            }
+            Console.WriteLine("Received from client: '{0}' with length {1}", receivedString, receivedString.Length.ToString());
 
-/*             if (File.Exists(receivedString)) {
+            if (File.Exists(receivedString)) {
                 outputFile = File.ReadAllBytes(receivedString);
-                int numberOfPackages = (int)Math.Ceiling((double)outputFile.Length/1000);
-                for (int i = 0; i < numberOfPackages; i++) {
-                    dataStream.Write(outputFile,i*1000,1000);
-                }
+                Console.WriteLine("File is {0} bytes long", outputFile.Length.ToString());
+                // int numberOfPackages = (int)Math.Ceiling((double)outputFile.Length/1000);
+                // for (int i = 0; i < numberOfPackages; i++) {
+                //     dataStream.Write(outputFile,i*1000,1000);
+                // }
+
+                // Send file size if it exists
+                int len = outputFile.Length;
+                outMsg = len.ToString();
+                byte[] outBuf = Encoding.ASCII.GetBytes(outMsg);
+                dataStream.Write(outBuf, 0, outBuf.Length);
+
+                bytesRead = dataStream.Read(recBuffer,0,recBuffer.Length); //Get acknowledgement
+                receivedString = Encoding.ASCII.GetString(recBuffer,0,bytesRead);
+                if (receivedString == "1") {// if ack is true
+                    Console.WriteLine("Received acknowledgement from client. Sending file");
+                    dataStream.Write(outputFile,0,outputFile.Length);
+                 }
             } else {
-                string msg = "err: file doesnt exist";
-                byte[] msgToSend = Encoding.ASCII.GetBytes(msg);
-                dataStream.Write(msgToSend, 0, msgToSend.Length);
-            } */
+                outBuf = Encoding.ASCII.GetBytes("0");
+                dataStream.Write(outBuf,0,outBuf.Length); //Write a 0 signaling file doesn't exist or is empty
+            }
 
             return clientHandler;
         }
